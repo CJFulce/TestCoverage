89917dff54bf40934177141abcb535262306a586
CJFulce <conner.fulciniti@bsu.edu>
2025-11-19 20:10:30 +0000

    Tests added to 100% coverage

--- AtmServices.Test/UnitTest1.cs
+++ AtmServices.Test/UnitTest1.cs
@@ -1,10 +1,49 @@
+using AtmServices;
+
 namespace AtmServices.Test;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void TestInitialBalance()
     {
+        var atm = new Atm(100);
+        Assert.Equal(100, atm.getBalance());
+    }
 
+    [Fact]
+    public void TestSuccessfulWithdraw()
+    {
+        var atm = new Atm(100);
+        bool result = atm.withdraw(50);
+        Assert.True(result);
+        Assert.Equal(50, atm.getBalance());
+    }
+
+    [Fact]
+    public void TestFailedWithdraw()
+    {
+        var atm = new Atm(100);
+        bool result = atm.withdraw(150);
+        Assert.False(result);
+        Assert.Equal(100, atm.getBalance());
+    }
+
+    [Fact]
+    public void TestSuccessfulDeposit()
+    {
+        var atm = new Atm(100);
+        bool result = atm.deposit(50);
+        Assert.True(result);
+        Assert.Equal(150, atm.getBalance());
+    }
+
+    [Fact]
+    public void TestNegativeDeposit()
+    {
+        var atm = new Atm(100);
+        bool result = atm.deposit(-50);
+        Assert.False(result);
+        Assert.Equal(100, atm.getBalance());
     }
 }
